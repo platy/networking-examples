19fa12fbd350f7f641429ef8b59676779f74c2f0
Mike Bush <mpbush@gmail.com>
2016-05-02 22:48:52 +0200

    Refactor the create script and add creation for the router

--- 1-basic-routing/create.sh
+++ 1-basic-routing/create.sh
@@ -1,14 +1,32 @@
 #! /bin/bash
 set -ex
 
+softRestart() {
+	VBoxManage controlvm $1 acpipowerbutton
+	until VBoxManage startvm --type=separate $1; do sleep 3; done
+}
+
+vmCopy() {
+	VBoxManage guestcontrol $1 --username root --password root copyto /Users/mike/workspace/networking-examples/1-basic-routing/$2 --target-directory $3
+}
+
+vmRun() {
+	VBoxManage guestcontrol $1 --username root --password root run -- $2
+}
+
 # Create echo server
 VBoxManage clonevm "Ubuntu ReadOnly" --snapshot=base --options=link --name="1-echo" --register
 VBoxManage modifyvm 1-echo --nic1=intnet --intnet1=1-remote --groups /NetTest/1-basic-routing
 VBoxManage startvm 1-echo --type=headless
-until VBoxManage guestcontrol 1-echo --username root --password root copyto /Users/mike/workspace/networking-examples/1-basic-routing/echo/interfaces --target-directory /etc/network/interfaces; do
-	sleep 5
-done
+until vmCopy 1-echo echo/interfaces /etc/network/interfaces; do	sleep 5; done
 sleep 1
-VBoxManage controlvm 1-echo acpipowerbutton
-until VBoxManage startvm 1-echo --type=gui; do sleep 1; done
+softRestart 1-echo
+
+# Create router
+VBoxManage clonevm "Ubuntu ReadOnly" --snapshot=base --options=link --name="1-router" --register
+VBoxManage modifyvm 1-router --nic1=intnet --intnet1=1-remote --nic2=intnet --intnet2=1-local --groups /NetTest/1-basic-routing
+VBoxManage startvm 1-router --type=headless
+until vmCopy 1-router router/interfaces /etc/network/interfaces; do sleep 5; done
+vmRun 1-router "/sbin/sysctl -w net.ipv4.ip_forward=1"
+softRestart 1-router
 
